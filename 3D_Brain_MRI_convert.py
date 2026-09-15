6eb049cbbe941bfce51ea724d9836476c2486bf6
PaulineMariaNagel <pauline.nagel@stud.plus.ac.at>
2026-09-15 13:26:55 +0200

    Update 3D_Brain_MRI_convert.py
    comments were adjusted

--- 3D_Brain_MRI_convert.py
+++ 3D_Brain_MRI_convert.py
@@ -7,117 +7,107 @@ Created on Mon Jun 15 21:47:27 2026
 
 #Import needed packages 
 import numpy as np
-import nibabel as nib # we need this because we have a nii.gz file
-import dash # need that for the dashboard later on
-from dash import dcc, html #self explanatory, schieberregler und layout zeugs
-from dash.dependencies import Input, Output #verbindet regler mit grafik
-import plotly.graph_objects as go # 3d grafik paket
-from skimage.measure import marching_cubes #algorithmus der aus pixeln eine 3d oberfläche macht
-from scipy import ndimage # bildverarbitung
+import nibabel as nib 
+import dash 
+from dash import dcc, html
+from dash.dependencies import Input, Output 
+import plotly.graph_objects as go 
+from skimage.measure import marching_cubes
+from scipy import ndimage
 
 
 #set path and load data
-NIFTI_PATH = r"C:\Users\pauli\OneDrive\Desktop\65924614.nii.gz"
+NIFTI_PATH = r"load_your_path"
 
 
 print("1. loads file") 
-nii_img = nib.load(NIFTI_PATH) # nifti plus meta data
-volume = nii_img.get_fdata() #only imaging data as 3d matrix
+nii_img = nib.load(NIFTI_PATH) 
+volume = nii_img.get_fdata()
 
 
 #PREP data
-if len(volume.shape) == 4: # nii file hat 4 dimensionen, so needed to put no 4 on 0 
+if len(volume.shape) == 4: # nii file has 4 Dimensions, so we need to put no 4 on 0 
     volume = volume[:, :, :, 0]
 
 
-# Downsampling as of lots of data, jeder 2 pixel in jede richtung
+# Downsampling 
 volume = volume[::2, ::2, ::2]
 
 
-# Normalisieren, dunkelster punkt = 0, hellster = 1
+# Normalisation
 volume = (volume - np.min(volume)) / (np.max(volume) - np.min(volume))
 
 
-# SKULL STRIPPING (in zukunft mit fsl oder matlab)
+# SKULL STRIPPING
 print("2. skull stripping")
 
-# Schritt A: Gewebe-Maske erstellen (Schwellenwert leicht angehoben), alles was dunkler ist als 0.34 fliegt raus (haut etc oft dunkler)
+# create mask, everything above 0.34 is excluded (skin, hair, etc, we only want brain tissue)
 tissue_mask = volume > 0.34
 
-# Schritt B: Gewebe wegschälen
+# erode mask
 eroded_mask = ndimage.binary_erosion(tissue_mask, iterations=2)
 
-# Schritt C: Größtes Objekt isolieren, weil biggest object is the brainy (hopefully)
+# isolate the biggest object, which should be the brain
 label_im, nb_labels = ndimage.label(eroded_mask)
 sizes = ndimage.sum(eroded_mask, label_im, range(nb_labels + 1))
 largest_label = np.argmax(sizes[1:]) + 1
 brain_mask = (label_im == largest_label)
 
-# Schritt D: Maske wieder auf Originalgröße bringen und Löcher im Inneren füllen
+# bring the mask back to its original size
 brain_mask = ndimage.binary_dilation(brain_mask, iterations=2)
 brain_mask = ndimage.binary_fill_holes(brain_mask)
 
-# Das final bereinigte Gehirn
+# now we should have the brain only
 brain_stripped = np.where(brain_mask, volume, 0)
 max_z = brain_stripped.shape[2] 
 
 
+# Check if it has worked!!!! if not the cutoff value (here .34) has to be adjusted) 
 
-
-
-#DEFNITELY DO CHECK BECAUSE ITS TRIAL AND ERROR
-
-app = dash.Dash(__name__) #Initialisiert Web Dashboard
+app = dash.Dash(__name__) 
 
 #Header
 app.layout = html.Div([
     html.H1("Data Handling Abschlussprojekt, 3D Brain", style={'textAlign': 'center', 'fontFamily': 'Arial', 'color': 'white'}),
     
-    html.Div([ #Öffnet eine neue Layout box links, brauchen wir für die Steuerung und den Schieber
+    html.Div([ #layout box
         html.Div([
             html.H3("Brain Segments"),
-            html.P("Skull stripped, only brain tissue and spinal cord can be seen"), #html P fügt einfach fließtext ein
+            html.P("Skull stripped, only brain tissue and spinal cord can be seen"), #text
             html.Br(), #Zeilenumbruch
             html.Label("Sagittale Schnitthöhe (Z-Achse):", style={'fontWeight': 'bold'}),
-            dcc.Slider( #startet bzw setzt de schieberregler auf
+            dcc.Slider( #starts the slider to go through the brain slices
                 id='slider', #Name the slider
                 min=0, 
-                max=max_z - 1, #max. wert wo man die "oberste Scicht vom hirn sehen kann"
-                value=max_z - 1,  #default wenn man ete öffnet, sodass das ganze hirn am anfang zu sehen ist
+                max=max_z - 1, #max. value of slider
+                value=max_z - 1,  #default for the beginning
                 step=1,
-                marks={i: {'label': str(i), 'style': {'color': 'white'}} for i in range(0, max_z, 10)}#mini schleife, erstellt 10er markierungen beim slider
+                marks={i: {'label': str(i), 'style': {'color': 'white'}} for i in range(0, max_z, 10)}
             ),
             html.Br(),
             html.P("Steuerung: Klicken und ziehen zum Rotieren", 
                    style={'fontSize': '12px', 'color': 'gray', 'marginTop': '30px'})
         ], style={'width': '25%', 'display': 'inline-block', 'verticalAlign': 'top', 'padding': '20px', 'backgroundColor': '#222', 'borderRadius': '10px', 'color': 'white'}),
-       #letze zeile schließt das kleine layout fenster, gesetzt das 25% vom gesamten bildschirm einnimmt, runde ecken und weiße schrift
-        
-        
-        
-        
-        
-        #nochmal neue layout box 
+       
+       #new layout box
         html.Div([
-            dcc.Graph(id='brain-mesh-graph', style={'height': '82vh'}) #fügt das grafikfenster ein, 82vh zwingt es dass 82% fensterhöhe
-        ], style={'width': '70%', 'display': 'inline-block', 'paddingLeft': '20px'}) #schließt die box wieder, nimmt 70% vom bildschirm ein, 20pixel abstand zu steuerung
+            dcc.Graph(id='brain-mesh-graph', style={'height': '82vh'}) #determines the size of the graphic
+        ], style={'width': '70%', 'display': 'inline-block', 'paddingLeft': '20px'}) 
     ], style={'display': 'flex'}) #needed so that both boxes, control and brainy remain together accordingly
 ], style={'padding': '20px', 'backgroundColor': '#111', 'minHeight': '100vh'})
-#schließt die Hauptbox, formatiert Innenabstand, bacground colour und sorgt dafür dass die seite so hoch wie der ganze bildschirm ist
 
-
-#Stuff to slide
-@app.callback( #sagt programm dass die Funktion automatisch aufgerufen werden soll
-    Output('brain-mesh-graph', 'figure'), #bestimmt ziel, nämlich 3d brainy
-    [Input('slider', 'value')] # bestimmt den auslöser, nämlich den schieberregler
+#slide functions
+@app.callback( #tells the programm that we need this function
+    Output('brain-mesh-graph', 'figure'), #sets the output, here the brain
+    [Input('slider', 'value')] #sets the input, here the slider value
 )
-def update_3d_cut(z_limit): #phyton function; startet funktion und übergibt schieber position als variable z_limit
-    cut_volume = brain_stripped.copy() #erstellt kopie vom mr datensatz damit das original nicht kaputt geht beim rendering
+def update_3d_cut(z_limit): 
+    cut_volume = brain_stripped.copy() #creates a copy of the brain so that potential errors in the rendering doesnt do damage 
     if z_limit < max_z - 1:
-        cut_volume[:, :, z_limit+1:] = 0 #wenn der slider nach unten bewegt wird, werden alle brain slices darüber auf 0 gesetzt. 
+        cut_volume[:, :, z_limit+1:] = 0 
         
     if np.max(cut_volume) == 0:
-        return go.Figure() #damit nichts abstürtzt (man lernt ausfehlern), wird wenn der regler auf 0 ist die leere Grafik angezeigt
+        return go.Figure() # so that if the slider reaches 0 the entire setup doesnt get stuck. 
 
     try:
         verts, faces, normals, values = marching_cubes(cut_volume, level=0.25)
@@ -125,33 +115,33 @@ def update_3d_cut(z_limit): #phyton function; startet funktion und übergibt sch
         return go.Figure() 
     
    
-    fig = go.Figure(data=[go.Mesh3d( #startet neue grafik
-        x=verts[:, 0],#mr koordinaten
+    fig = go.Figure(data=[go.Mesh3d( 
+        x=verts[:, 0],#mr coordinates for display
         y=verts[:, 2],
-        z=max_z - verts[:, 1], #z ist gespiegelt weil brainy falsch stand
-        i=faces[:, 0], #teilt plotly mit wie die punkte miteinander zu dreiecken verbunden werden müssen damit wir eine smoothe oberfläche haben
+        z=max_z - verts[:, 1], 
+        i=faces[:, 0], #tells plotly how the points have to be connected to create a smoother display/surface
         j=faces[:, 1],
         k=faces[:, 2],
-        intensity=values,      # mr signalwerte (Helligkeitswerte)
+        intensity=values,      # mr values
         colorscale='mrybm',    # turns it into colour
-        opacity=1.0, #blickdichtes brainy
-        flatshading=False,     #leichte schattierung damit realistisch
+        opacity=1.0, 
+        flatshading=False,     # create shadowing
         lighting=dict(ambient=0.4, diffuse=0.8, specular=0.3, roughness=0.4),
-        lightposition=dict(x=100, y=100, z=100) #richtet virtuelle scheinwerfer ein damit wir was sehen können
+        lightposition=dict(x=100, y=100, z=100) #light for a nice display
     )])
     
     
     
     
     fig.update_layout(
-        scene=dict( #startet die 3d szene (würfel in dem brainy liegen soll)
+        scene=dict( 
             xaxis=dict(visible=False), 
             yaxis=dict(visible=False),
             zaxis=dict(visible=False),
             aspectmode='data', #assures original proportions from mri
             bgcolor='black'
         ),
-        margin=dict(l=0, r=0, b=0, t=0), #ränder auf null damit grafik viel platz hat
+        margin=dict(l=0, r=0, b=0, t=0), 
         template='plotly_dark' #darkmode
     )
     
